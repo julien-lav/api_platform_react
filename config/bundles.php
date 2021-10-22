5ce1ce564171b66d61c4799e2fd08b795dd1804d
Julien Laville <j.laville@claravista.dom>
2021-10-22 18:10:46 +0200

    react

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
